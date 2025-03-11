59320c927e4d75f4f1e6b73ac8bdeac4d16c6396
ProgSomel <somelahmed55@gmail.com>
2025-03-11 06:54:57 +0600

    swap variables via 3 methods

---
+++ Master the Basics/js/script.js
@@ -0,0 +1,46 @@
+//! Accept and print answer
+// let age = prompt('What is your age? ');
+// console.log(age); //?Here age will become string .... because prompt will accept it as string
+// --------------------------------------- 
+// let age = Number(prompt("What is your age? ")); //?type casting or type conversion 
+// console.log(age); //?Here age will become number
+// ----------------------------------------
+// let age = prompt('Enter your age: ');
+// age = Number(age);
+// console.log(age);25
+// ----------------------------------------
+
+//! Swap two variables via 3 methods 
+//? 1. Swap with extra variable 
+// let a = 10;
+// let b = 20;
+// console.log(`Before swapping a is ${a} and b is ${b}`);
+// let c = a; //? c = 10, a = 10
+// a = b; //? a = 20
+// b = c; //? b = 10, c = 10
+// console.log(`After swapping a is ${a} and b is ${b}`);
+
+//? 2. With mathmetic formula 
+// let a = 10;
+// let b = 20;
+// console.log(`Before swapping a is ${a} and b is ${b}`);
+// a = a + b; //? a = 30
+// b = a - b; //? b = 30 - 20 = 10
+// a = a - b; //? a = 30 - 10 = 20;
+// console.log(`After swapping a is ${a} and b is ${b}`);
+
+//? 3. With destructuring assignment
+// let a = 10;
+// let b = 20;
+// console.log(`Before swapping a is ${a} and b is ${b}`);
+// [a, b] = [b, a];
+// console.log(`After swapping a is ${a} and b is ${b}`);
+
+
+
+
+
+
+
+
+
